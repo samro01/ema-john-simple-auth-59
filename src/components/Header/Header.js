528d06d17437e84ac67ebfd73da9becbdada73a8
samro01 <srozario22@gmail.com>
2022-10-18 10:53:52 +0200

    user context modified

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+
 import logo from '../../images/Logo.svg';
 import './Header.css';
 
 const Header = () => {
+
+
     return (
         <nav className='header'>
             <img src={logo} alt="" />
@@ -12,6 +15,9 @@ const Header = () => {
                 <Link to="/orders">Orders</Link>
                 <Link to="/inventory">Inventory</Link>
                 <Link to="/about">About</Link>
+                <Link to="/login">Login</Link>
+                <Link to="/signup">SignUp</Link>
+
             </div>
         </nav>
     );
